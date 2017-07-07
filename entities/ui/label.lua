3f6b2b6c1a9cc0ae66009ef0336ba5a8675f87b3
lzubiaur <zubiaur.laurent@gmail.com>
2017-07-07 15:03:23 +0200

    Add touch test

---
+++ entities/ui/label.lua
@@ -0,0 +1,25 @@
+-- Label.lua
+
+local Entity = require 'entities.base.entity'
+
+local Label = Class('Label',Entity):include(Stateful)
+
+function Label:initialize(world, x,y, text, opt)
+  opt = opt or {}
+  opt.zOrder = opt.zOrder or 10
+  self.text = text or 'label'
+  self.color = opt.color or {255,255,255,255}
+
+  local w = g.getFont():getWidth(self.text)
+  local h = g.getFont():getHeight()
+  self.limit = opt.limit or w
+
+  Entity.initialize(self,world, x,y, w,h, opt)
+end
+
+function Label:draw()
+  g.setColor(unpack(self.color))
+  g.printf(self.text,self.x,self.y-self.h/2,self.limit,'center')
+end
+
+return Label
