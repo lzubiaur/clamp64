49bd786dec196434c35a778f3974e441b1384f29
lzubiaur <zubiaur.laurent@gmail.com>
2017-07-19 07:15:52 +0200

    Fix collision filter

--- entities/enemy.lua
+++ entities/enemy.lua
@@ -6,13 +6,15 @@ local Enemy = Class('Enemy', Body)
 
 function Enemy:initialize(world,x,y)
   local w,h = game.visible:pointToPixel(10,10)
-  Body.initialize(self,world,x,y,w,h,{ vx=100,vy=100, zOrder = -1 })
+  Body.initialize(self,world,x,y,w,h,{ vx=100,vy=50, zOrder = -1 })
 end
 
 function Enemy:update(dt)
   local cx,cy = self:getCenter()
   self:applyVelocity(dt)
-  local cols,len = self:move(self.x,self.y)
+  local cols,len = self:move(self.x,self.y,function(self,other)
+    return other.class.name == 'Segment' and 'bounce' or nil
+  end)
   for i=1,len do
     local n = cols[i].normal
     self:applyCollisionNormal(self.x*n.x,self.y*n.y,1)
