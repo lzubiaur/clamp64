ff12cc8051e67098402639157c98d56ff0f160f7
lzubiaur <zubiaur.laurent@gmail.com>
2017-07-25 15:23:52 +0200

    Add game over

--- entities/enemy.lua
+++ entities/enemy.lua
@@ -9,15 +9,27 @@ function Enemy:initialize(world,x,y)
   Body.initialize(self,world,x,y,w,h,{ vx=100,vy=50, zOrder = -1 })
 end
 
+function Enemy:filter(other)
+  if other.class.name == 'Segment' then
+    return 'bounce'
+  elseif other.class.name == 'Player' then
+    return 'touch'
+  end
+  return nil
+end
+
 function Enemy:update(dt)
   local cx,cy = self:getCenter()
   self:applyVelocity(dt)
-  local cols,len = self:move(self.x,self.y,function(self,other)
-    return other.class.name == 'Segment' and 'bounce' or nil
-  end)
+  local cols,len = self:move(self.x,self.y,Enemy.filter)
   for i=1,len do
-    local n = cols[i].normal
-    self:applyCollisionNormal(self.x*n.x,self.y*n.y,1)
+    local other = cols[i].other
+    if other.class.name == 'Segment' then
+      local n = cols[i].normal
+      self:applyCollisionNormal(self.x*n.x,self.y*n.y,1)
+    elseif other.class.name == 'Player' then
+      Beholder.trigger('GameOver',self)
+    end
   end
 end
 
