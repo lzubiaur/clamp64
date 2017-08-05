a610e69ea7293f8042174a91a641280abe7cfc99
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-05 22:06:08 +0200

    Remove invincibility

--- entities/blink.lua
+++ entities/blink.lua
@@ -8,17 +8,28 @@ local Blink = Player:addState('Blink')
 
 function Blink:enteredState()
   self.isInvincible = true
-  local anim = Animated:new(Assets.img.tilesheet,0,0,12,12)
-  anim:setAnimation(game.tilesheetGrid(1,1,2,3),.08)
-  self.ship:addChild(anim)
-  Timer.after(3,function()
-    self.ship:removeChild(anim)
-    anim:destroy()
-    self:popState()
+  self.anim = self:createAnimation()
+  Timer.after(3,function() self:popState() end)
+  -- Use self.anim to group handler because self is already used by player state
+  Beholder.group(self.anim,function()
+    Beholder.observe('right',function() self:popState() end)
+    Beholder.observe('left',function() self:popState() end)
+    Beholder.observe('up',function() self:popState() end)
+    Beholder.observe('down',function() self:popState() end)
   end)
 end
 
+function Blink:createAnimation()
+  local anim = Animated:new(Assets.img.tilesheet,0,0,12,12)
+  anim:setAnimation(game.tilesheetGrid(1,1,2,3),.08)
+  self.ship:addChild(anim)
+  return anim
+end
+
 function Blink:exitedState()
+  Beholder.stopObserving(self.anim)
+  self.ship:removeChild(self.anim)
+  self.anim:destroy()
   Beholder.trigger('beatable')
   self.isInvincible = false
 end
