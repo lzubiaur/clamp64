a890f25ae4a6e5423439e13b24549829ef1070fd
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-05 09:10:43 +0200

    Fix checkpoint

--- common/checkpoint.lua
+++ common/checkpoint.lua
@@ -12,16 +12,18 @@ end
 
 function Checkpoint:trigger()
   local t = {}
-  Beholder.trigger('checkpoint',t)
+  local count = Beholder.trigger('checkpoint',t)
+  if not count or count < 1 then return end -- Player might not exists (e.g. killed)
   table.insert(self.pos,t)
   if #self.pos > 10 then
     table.remove(self.pos,1)
   end
+  Log.debug('Checkpoint',t[1],t[2])
 end
 
 function Checkpoint:getLastPosition()
   local p = self.pos
-  for i=1,#p do
+  for i=#p,1,-1 do
     local x,y = p[i][1],p[i][2]
     local item,len = self.world:queryPoint(x,y)
     if len == 0 then return x,y end
