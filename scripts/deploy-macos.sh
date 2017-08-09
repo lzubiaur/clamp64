04c288dcd63d898b5dfb3502dec2477661e4709d
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-09 12:56:24 +0200

    Edit info.plist and zip the bundle

--- scripts/deploy-macos.sh
+++ scripts/deploy-macos.sh
@@ -7,6 +7,9 @@ LOVE_MACOS=../love-macos
 # debug/release
 BUILD=debug
 
+BUNDLE_ID="com.voodoocactus.games.clamp"
+BUNDLE_NAME="Clamp"
+
 FILES="common
 entities
 gamestates
@@ -21,15 +24,25 @@ tests
 # Write the debug/release configuration
 echo "return '$BUILD'" > common/build.lua
 
-# Create the game zip. Must not contain the root folder
-rm $LOVE_MACOS/love.app/Contents/Resources/game.love
-zip -r -y $LOVE_MACOS/love.app/Contents/Resources/game.love $FILES -x *.DS_Store
+BUNDLE="$LOVE_MACOS/$BUNDLE_NAME.app"
+cp -r "$LOVE_MACOS/love.app" $BUNDLE
 
-cp libpolyclipping.dylib $LOVE_MACOS/love.app/Contents/Frameworks
+# Create the game zip. Must not contain the root folder
+zip -r $BUNDLE/Contents/Resources/game.love $FILES -x *.DS_Store
+
+# Install Frameworks
+cp libpolyclipping.dylib $BUNDLE/Contents/Frameworks
 
 pushd $LOVE_MACOS
 
-# TODO edit plist
+PLIST="$BUNDLE_NAME.app/Contents/Info.plist"
+
+# Update Info.plist
+plutil -remove UTExportedTypeDeclarations $PLIST
+plutil -replace CFBundleIdentifier -string "$BUNDLE_ID" $PLIST
+plutil -replace CFBundleName -string "$BUNDLE_NAME" $PLIST
+
+zip -ry "$BUNDLE_NAME.osx.zip" "$BUNDLE_NAME.app"
 
 open love.app
 
