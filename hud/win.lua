f50162cdbcbc9b1b7f9a75d506699a26855d9c54
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-12 18:03:13 +0200

    Add labels and layer

--- hud/win.lua
+++ hud/win.lua
@@ -1,8 +1,10 @@
 -- hud/win.lua
 
 local HUD = require 'hud.base'
-local Button = require 'entities.ui.button'
-local ImageButton = require 'entities.ui.imagebutton'
+local Label = require 'entities.ui.label'
+local Quad = require 'entities.base.quad'
+local Node = require 'entities.base.node'
+local ColorLayer = require 'entities.base.colorlayer'
 
 local Win = HUD:addState('Win')
 
@@ -10,31 +12,47 @@ function Win:enteredState()
   self:createSwallowTouchLayer(function()
     Beholder.trigger('NextLevel')
   end)
-  -- Quit gameplay (goto main menu)
-  -- self.back = ImageButton:new(self.world,0,0,{
-  --   os = 1, oy = 1,
-  --   image = Assets.img.quit,
-  --   onSelected = function()
-  --     Beholder.trigger('GotoMainMenu')
-  --     self:popState()
-  --   end,
-  --   color = {255,255,255,255}
-  -- })
 
-  -- Next level button
-  -- Button:new(self.world,18,30,28,10,{
-  --   os = 1, oy = 1, corner = 4,
-  --   text = 'Next',
-  --   onSelected = function()
-  --     Beholder.trigger('NextLevel')
-  --   end,
-  -- })
+  self:createDitherLayer()
 
+  Label:new(self.world,0,10,'Level '..game.state.cli..' cleared',{limit=64})
+
+  local sl = self.node:addChild(Label:new(self.world,0,25,'Score:',{limit=64}))
+  sl.score = 0
+  sl.update = function(self,dt)
+    self.text = 'Score:'..math.ceil(self.score)
+  end
+  self.timer:script(function(wait)
+    self.tween = Tween.new(1,sl,{ score = game:getGrandScore() })
+    wait(1)
+    local level = game:getCurrentLevelState()
+    local label = Label:new(self.world,0,40,'x0')
+    for i=1,level.diamonds do
+      label.text = 'x'..i
+      wait(.3)
+    end
+  end)
+end
+
+function Win:createDitherLayer()
+  self.node:addChild(ColorLayer:new(0,0,64,64,{color={0,30,63,120}}))
+  local ditter = Assets.img.ditter
+  ditter:setWrap('repeat','repeat')
+  local quad = g.newQuad(0,0,64,64,ditter:getDimensions())
+  quad = Quad:new(ditter,quad,0,0,{color={0,30,63,100},ax=0,ay=0})
+  self.node:addChild(quad)
+end
+
+function Win:update(dt)
+  if self.tween and self.tween:update(dt) then
+    self.tween = nil
+  end
+  HUD.update(self,dt)
 end
 
 function Win:exitedState()
+  self.timer:clear()
   self:removeSwallowTouchLayer()
-  -- self.back:destroy()
 end
 
 return Win
