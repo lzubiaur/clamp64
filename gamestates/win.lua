4136afd721953fab5e2cf0cc0f0ba1734d8ec2e3
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-12 18:02:50 +0200

    Clean up

--- gamestates/win.lua
+++ gamestates/win.lua
@@ -6,11 +6,7 @@ local Win = Game:addState('Win')
 
 function Win:enteredState(a,b)
   Log.debug('Entered state Win')
-  Beholder.trigger('Win')
-  self.img = Assets.img.ditter
-  self.img:setWrap('repeat','repeat')
-  local x,y,w,h = self.visible:screen()
-  self.quad = g.newQuad(x,y,w,h,self.img:getDimensions())
+  -- Beholder.trigger('Win')
   self.hud:gotoState('Win')
 end
 
@@ -19,6 +15,8 @@ function Win:exitedState()
 end
 
 function Win:update(dt)
+  -- Only update HUD
+  self.hud:update(dt)
 end
 
 -- Disable touch controls (e.g. moving player) but still call
@@ -29,19 +27,6 @@ end
 
 function Win:released(x,y)
   Game.released(self,x,y)
-end
-
-function Win:drawAfterCamera(l,t,w,h)
-  g.setColor(0,30,63,120)
-  g.rectangle('fill',0,0,conf.sw,conf.sh)
-  g.setColor(0,30,63,100)
-  g.draw(self.img,self.quad,self.visible:leftTop())
-  g.setColor(255,241,232,255)
-  g.printf(
-    {{255,241,232,255},'Level '..self.state.cli..' cleared!\n',
-     {255,163,0,255},'Score:'..self:getGrandScore()},
-    0,conf.sh/2-self.fontHeight/2-20,conf.sw,'center')
-  self.hud:draw(l,t,w,h)
 end
 
 function Win:keypressed(key, scancode, isrepeat)
