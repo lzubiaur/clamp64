67e079b374cc6ee4b4210d04f3bb172b52eec0c5
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-13 09:40:56 +0200

    Add sounds and fade out

--- entities/diamond.lua
+++ entities/diamond.lua
@@ -12,11 +12,20 @@ function Diamond:initialize(world,x,y,w,h)
   local timer = Timer.every(.3,function() quad:setVisible(not quad:isVisible()) end)
   Beholder.group(self,function()
     Beholder.observe('diamond',self,function()
-      Timer.cancel(timer)
-      love.audio.play(Assets.sounds.sfx_coin_cluster3:clone())
-      self:destroy()
+      if not self.tween then
+        quad:setVisible(true)
+        Timer.cancel(timer)
+        love.audio.play(Assets.sounds.sfx_coin_cluster3)
+        self.tween = Tween.new(.8,quad,{sx=3,sy=3,color={255,255,255,0}})
+      end
     end)
   end)
+end
+
+function Diamond:update(dt)
+  if self.tween and self.tween:update(dt) then
+    self:destroy()
+  end
 end
 
 return Diamond
