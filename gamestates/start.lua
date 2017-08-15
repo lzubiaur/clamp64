4c5d668f59e13902104fe23a701204bcd01dbe15
lzubiaur <zubiaur.laurent@gmail.com>
2017-08-15 19:02:18 +0200

    Add stars particle

--- gamestates/start.lua
+++ gamestates/start.lua
@@ -35,6 +35,17 @@ function Start:enteredState()
   self:fadeIn(function()
     Push:setShader()
   end)
+
+  self.ps = g.newParticleSystem(Assets.img.star,100)
+  self.ps:setParticleLifetime(1)
+  self.ps:setLinearAcceleration(0,0,10,10)
+  self.ps:setAreaSpread('normal',32,32)
+  self.ps:setLinearDamping(0,-2)
+  self.ps:setRadialAcceleration(20,50)
+  self.sx,self.sy = .05,.05
+  self.tween = Tween.new(.5,self,{sx=1,sy=1})
+  self.ps:emit(32)
+  self.ps:setColors({32,51,123},{255,241,232})
 end
 
 function Start:exitedState()
@@ -59,8 +70,17 @@ function Start:keypressed(key, scancode, isRepeat)
   end
 end
 
+function Start:update(dt)
+  self.ps:update(dt)
+  Game.update(self,dt)
+  self.ps:emit(1)
+  self.tween:update(dt)
+end
+
 function Start:drawBeforeCamera()
-  g.draw(Assets.img.mainmenu,0,0)
+  g.draw(Assets.img.bg,0,0)
+  g.draw(self.ps,32,22)
+  g.draw(Assets.img.logo,32,32,0,self.sx,self.sy,32,32)
 end
 
 return Start
